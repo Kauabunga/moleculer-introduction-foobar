a81ec8d43be2e778332fb2f6fe993d23099277dc
Carson <carson.bruce@gmail.com>
2020-11-25 15:31:27 +1300

    moved source into subfolder

--- src/lib/broker/init.js
+++ src/lib/broker/init.js
@@ -4,13 +4,13 @@ exports.initGatewayBroker = initGatewayBroker;
 
 function initGatewayBroker(ApiGateway, services = []) {
 	return (settings, brokerSettings = {}) => {
-		console.time(`Setup gateway broker`); // eslint-disable-line
+		console.time(`Setup gateway broker`);
 
 		const broker = new ServiceBroker(constructBrokerSettings(brokerSettings));
 		const gatewayService = broker.createService(ApiGateway, { settings });
 		services.forEach((service) => broker.createService(service));
 
-		console.timeEnd(`Setup gateway broker`); // eslint-disable-line
+		console.timeEnd(`Setup gateway broker`);
 
 		return [broker, gatewayService];
 	};
